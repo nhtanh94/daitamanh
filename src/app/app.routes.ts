1a55b633d4b1b23d5222c02b70899d5b3aab0746
anhnht94 <anhnht@youmed.vn>
2024-03-13 21:05:50 +0700

    SOME UPDATE

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { NewsComponent } from './pages/news/news.component';
 
-export const routes: Routes = [{path:'',component:HomeComponent}];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'san-pham', component: ProductDetailComponent },
+    { path: 'tin-tuc', component: NewsComponent },
+    { path: 'gioi-thieu', component: AboutComponent },
+    { path: '404', component: NotFoundComponent }
+];
